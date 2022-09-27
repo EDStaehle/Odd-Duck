5a12c3e9e3709fab9e029e97597f265b26bb6aa0
Elias <elias.staehle@gmail.com>
2022-09-27 19:37:02 -0400

    lab 12 completed replaced ul with a chart

--- JS/index.js
+++ JS/index.js
@@ -9,8 +9,8 @@ let imgOne = document.getElementById('img-one');
 let imgTwo = document.getElementById('img-two');
 let imgThree = document.getElementById('img-three');
 
-let resultsBtn = document.getElementById('show-results-btn');
-let resultsContainer = document.getElementById('results-container');
+
+
 
 
 function Product(name, fileExtension = 'jpg') {
@@ -26,16 +26,17 @@ function randomIndex() {
   return Math.floor(Math.random() * prodArray.length);
 }
 
-
+let ranNum = [];
 function renderImgs() {
-  let imgOneI = randomIndex();
-  let imgTwoI = randomIndex();
-  let imgThreeI = randomIndex();
-
-  while (imgOneI === imgTwoI || imgTwoI === imgThreeI || imgOneI === imgThreeI) {
-    imgTwoI = randomIndex();
-    imgThreeI = randomIndex();
+  while (ranNum.length < 6) {
+    let imgRend = randomIndex();
+    if (!ranNum.includes(imgRend)) {
+      ranNum.push(imgRend);
+    }
   }
+  let imgOneI = ranNum.shift();
+  let imgTwoI = ranNum.shift();
+  let imgThreeI = ranNum.shift();
 
   imgOne.src = prodArray[imgOneI].img;
   imgTwo.src = prodArray[imgTwoI].img;
@@ -55,36 +56,84 @@ function handleClick(event) {
   let imgClicked = event.target.alt;
 
 
-  console.log('img clicked >>', imgClicked);
 
   for (let i = 0; i < prodArray.length; i++) {
     if (prodArray[i].name === imgClicked) {
 
       prodArray[i].clicks++;
+      break;
     }
   }
 
   votesLeft--;
 
-  renderImgs();
-
   if (votesLeft === 0) {
     imgContainer.removeEventListener('click', handleClick);
+    renderChart();
+  } else {
+    renderImgs();
   }
 }
-
-function handleShowResults() {
-
-  if (votesLeft === 0) {
-    for (let i = 0; i < prodArray.length; i++) {
-      let liElem = document.createElement('li');
-      liElem.innerHTML = `<span class='nameOfImg'>${prodArray[i].name}</span><br><span class='views'>viewed: ${prodArray[i].views}</span> times<span class='clicks'> clicked: ${prodArray[i].clicks}</span> times`;
-      resultsContainer.appendChild(liElem);
-    }
-    resultsBtn.removeEventListener('click', handleShowResults);
+let canvasChart = document.getElementById('myChart').getContext('2d');
+const gradientView = canvasChart.createLinearGradient(0, 0, 0, 400);
+gradientView.addColorStop(0.3, 'rgba(131,58,180,.7)');
+gradientView.addColorStop(.6, 'rgba(233,23,23,.7)');
+gradientView.addColorStop(.9, 'rgba(252,176,69,.7)');
+const gradientClick = canvasChart.createLinearGradient(0, 0, 0, 400);
+gradientClick.addColorStop(.14, 'rgba(252,0,0,.7)');
+gradientClick.addColorStop(.28, 'rgba(70,4,53,.7)');
+gradientClick.addColorStop(.42, 'rgba(15,121,9,.7)');
+gradientClick.addColorStop(.56, 'rgba(8,34,138,.7)');
+gradientClick.addColorStop(.70, 'rgba(5,179,166,.7)');
+gradientClick.addColorStop(.84, 'rgba(222,117,2,.7)');
+gradientClick.addColorStop(.98, 'rgba(0,212,255,.7)');
+function renderChart() {
+  let prodNames = [];
+  let prodLikes = [];
+  let prodViews = [];
+  for (let i = 0; i < prodArray.length; i++) {
+    prodNames.push(prodArray[i].name);
+    prodLikes.push(prodArray[i].clicks);
+    prodViews.push(prodArray[i].views);
   }
-}
 
+
+  const data = {
+    labels: prodNames,
+    datasets: [{
+      label: 'Votes',
+      data: prodLikes,
+      backgroundColor: gradientClick,
+      borderColor: [
+        'rgb(255, 99, 132)'
+      ],
+      borderWidth: 1
+    },
+    {
+      label: 'Views',
+      data: prodViews,
+      backgroundColor: gradientView,
+      borderColor: [
+        'rgb(255, 159, 64)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
+  };
+  // eslint-disable-next-line no-unused-vars, no-undef
+  const myChart = new Chart(canvasChart, config);
+}
 new Product('sweep', 'png');
 new Product('bag');
 new Product('banana');
@@ -105,8 +154,7 @@ new Product('unicorn');
 new Product('water-can');
 new Product('wine-glass');
 
-renderImgs();
 
+renderImgs();
 imgContainer.addEventListener('click', handleClick);
-resultsBtn.addEventListener('click', handleShowResults);
 
